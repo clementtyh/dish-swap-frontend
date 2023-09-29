364234c9be57f0d7086af739db09dea47b316c48
maryam adn <maryamahmaddahlan@gmail.com>
2023-09-29 23:01:16 +0800

    fixed recipes page if no return (error 404)

--- src/components/CardsGrid.tsx
+++ src/components/CardsGrid.tsx
@@ -9,9 +9,9 @@ interface CardsGridProps {
 function CardsGrid({ cards }: CardsGridProps) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-16">
-      {cards.map((card) => (
+      {Array.isArray(cards) ? cards.map((card) => (
         <Card key={card._id} {...card} />
-      ))}
+      )) : "No available recipes currently"}
     </div>
   );
 }
