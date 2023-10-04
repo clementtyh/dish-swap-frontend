ec7166531d1dc539faf312eab930e535500dbfa5
vxncetxn <thevancetan@gmail.com>
2023-10-04 16:55:17 +0800

    Updated useDecoded to account for non-auth cases

--- src/utils/useDecoded.ts
+++ src/utils/useDecoded.ts
@@ -4,7 +4,7 @@ import { jwt_decode, JwtClaims } from "jwt-decode-es";
 export const useDecoded = (
   token: string
 ): [JwtClaims, (newToken: string) => void] => {
-  const [decoded, setDecodedRaw] = useState(jwt_decode(token));
+  const [decoded, setDecodedRaw] = useState(token ? jwt_decode(token) : {});
   const setDecoded = (newToken: string): void =>
     setDecodedRaw(jwt_decode(newToken));
 
