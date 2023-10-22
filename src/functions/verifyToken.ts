c861f1719c344eb4336a73c8aaa6902499d923be
maryam adn <maryamahmaddahlan@gmail.com>
2023-10-22 23:45:10 +0800

    editing verifytoken

--- src/functions/verifyToken.ts
+++ src/functions/verifyToken.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 import urlcat from "urlcat";
+import * as fs from "fs";
+import jwt from "jsonwebtoken";
+import { result } from "lodash";
 
 const SERVER = import.meta.env.PROD
   ? import.meta.env.VITE_API_URL_PROD
@@ -14,18 +17,32 @@ const verifyToken = async () => {
     return false;
   }
 
-  const result = await axios
-        .post(url, {}, {
-          headers: { Authorization: "Bearer " + token },
-        })
-        .then(() => {
-          return true;
-        })
-        .catch(() => {
-          return false;
-        })
-        
-  return result;
+  const resultBackend = await axios
+    .post(
+      url,
+      {},
+      {
+        headers: { Authorization: "Bearer " + token },
+      }
+    )
+    .then(() => {
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
+
+  // const publicKeyPath = import.meta.env.VITE_JWT_PUBLIC_KEY_PATH;
+  
+  // const publicKey = fs.readFileSync(publicKeyPath);
+  // jwt.verify(token, publicKey, { algorithms: ["RS256"] }, function (err, payload) {
+  //   if (err) {
+  //     console.log(err.name, ": ", err.message)
+  //   }
+  // });
+
+  // return resultBackend && resultFrontend;
+  return resultBackend;
 };
 
 export default verifyToken;
