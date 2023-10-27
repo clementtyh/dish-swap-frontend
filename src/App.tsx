390b444ecb6d9792182c96723f467d2845f89154
lxe819 <abeta8352@gmail.com>
2023-10-27 19:12:08 +0800

    Added Settings page and included the routing in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Recipe from "./Pages/Recipe.js";
 
 import "./index.css";
 import Profile from "./Pages/Profile.js";
+import Settings from "./Pages/Settings.js";
 
 function App() {
   const [isTokenValid, setIsTokenValid] = useState(false);
@@ -39,6 +40,10 @@ function App() {
         {
           path: "profile",
           element: <Profile isTokenValid={isTokenValid} setIsTokenValid={setIsTokenValid}/>,
+        },
+        {
+          path: "settings",
+          element: <Settings isTokenValid={isTokenValid} setIsTokenValid={setIsTokenValid}/>,
         },
         {
           path: "recipe/:recipeId",
