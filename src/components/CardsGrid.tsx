1ee955a88497a0da58f21e145d5c36beeb0c99a0
maryam adn <maryamahmaddahlan@gmail.com>
2023-11-06 09:17:40 +0800

    removed i

--- src/components/CardsGrid.tsx
+++ src/components/CardsGrid.tsx
@@ -9,7 +9,7 @@ interface CardsGridProps {
 function CardsGrid({ cards }: CardsGridProps) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 lg:gap-10 xl:gap-16">
-      {Array.isArray(cards) ? cards.map((card, i) => (
+      {Array.isArray(cards) ? cards.map((card) => (
         <Card key={card._id} {...card} />
       )) : "No available recipes currently"}
     </div>
